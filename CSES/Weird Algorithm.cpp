c277bc9862e47b29694c9bdc86e3e37fbdd00f2b
Olav Fosse <fosseolav@gmail.com>
2020-06-17 03:49:48 +0200

    Add "Weird Algorithm"

---
+++ CSES/Weird Algorithm.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  // https://stackoverflow.com/questions/31162367
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  long long int n; cin >> n;
+
+
+  cout << n << " ";
+  while(n!=1) {
+    if(n%2==0){
+      n/=2;
+    } else {
+      n = n*3 +1;
+    }
+    cout << n << " ";
+  }
+}
